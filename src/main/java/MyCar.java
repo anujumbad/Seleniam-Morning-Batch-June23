c688b6088c6171ea0a25eda9f8f68287cd743aca
anujumbad <anujumbad12@gmail.com>
2023-07-11 11:26:33 +0530

    My demo

--- src/main/java/MyCar.java
+++ src/main/java/MyCar.java
@@ -1,5 +1,6 @@
 public class MyCar {
     public static void main(String[] args) {
         System.out.println("My Car");
+        System.out.println("update my car");
     }
 }
---
+++ src/main/java/corejava/simpleprograms/TestingDemo.java
@@ -0,0 +1,7 @@
+package corejava.simpleprograms;
+
+public class TestingDemo {
+    public static void main(String[] args) {
+        System.out.println("My demo");
+    }
+}
