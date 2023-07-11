3daed5f625f0bdd800588dc25eccfbf415c7ad65
anujumbad <anujumbad12@gmail.com>
2023-07-11 07:14:54 +0530

    This is my second commit

--- src/main/java/corejava/simpleprograms/HelloWorld.java
+++ src/main/java/corejava/simpleprograms/HelloWorld.java
@@ -3,5 +3,6 @@ package corejava.simpleprograms;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("This is my second statment");
     }
 }
