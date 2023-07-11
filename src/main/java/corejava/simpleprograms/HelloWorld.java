1c8ee1b0f006d770c0455e89d5fc9bbab1eb71b3
anujumbad <anujumbad12@gmail.com>
2023-07-11 08:54:08 +0530

    This is my third statment

--- src/main/java/corejava/simpleprograms/HelloWorld.java
+++ src/main/java/corejava/simpleprograms/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("This is my second statment");
+        System.out.println("This is my third statment");
     }
 }
